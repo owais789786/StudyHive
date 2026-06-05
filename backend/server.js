80e7932fdd2bfb8f8099bec6804e3bf96c660fe0
unknown <owaissajjad789@gmail.com>
2026-06-05 22:50:30 +0500

    setting up socket.io

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,23 @@
 const app = require('./src/app');
 const connectDB = require('./src/config/db');
+const { Server } = require('socket.io');
+const { createServer } = require('http')
+
+const httpServer = createServer(app);
+const io = new Server(httpServer,{
+    cors:{
+        origin: process.env.CLIENT_URL || 'http://localhost:5173',
+        methods: ['GET','POST']
+    }
+})
+
+io.on('connection',(socket)=>{
+    console.log('user connected:' ,socket.id);
+    
+})
 
 connectDB.then(() => {
-    app.listen(process.env.PORT, () => {
-        console.log(`Server is Running at port : ${process.env.PORT}`);
-    })
+
 }).catch(error => {
     console.log("Failed to start server :" + error);
 })
